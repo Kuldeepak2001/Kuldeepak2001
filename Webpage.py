55a6602e8cf5729be79c4bb25f06339592965f2f
Anmol <anmol.rastogi@leadsquared.com>
2022-05-29 12:49:31 +0530

    Update for images

--- Webpage.py
+++ Webpage.py
@@ -10,7 +10,9 @@ window.geometry('550x500')
 def run():
     os.system('python C:/Users/anmol.r/PycharmProjects/FaceRecognition/face_recog.py')
 
-btn = Button(window, text="Click Me", bg="green", fg="white",command=run)
-btn.grid(column=0, row=0)
+btn = Button(window, text="Mark Attendance!", bg="green", fg="white",command=run)
+exit_button = Button(window, text="Exit", bg="green", fg="white", command=window.destroy)
+btn.grid(column=5, row=5)
+exit_button.grid(column=6, row=6)
 
 window.mainloop()
